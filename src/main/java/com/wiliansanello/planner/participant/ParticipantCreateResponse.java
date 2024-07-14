8553613a90687f28600890fb11a1cbef5dba81bc
wiliansanello <wiliansanello@outlook.com>
2024-07-14 15:07:23 -0300

    Aula 02

---
+++ src/main/java/com/wiliansanello/planner/participant/ParticipantCreateResponse.java
@@ -0,0 +1,6 @@
+package com.wiliansanello.planner.participant;
+
+import java.util.UUID;
+
+public record ParticipantCreateResponse(UUID id) {
+}
---
+++ src/main/java/com/wiliansanello/planner/participant/ParticipantRequestPayload.java
@@ -0,0 +1,4 @@
+package com.wiliansanello.planner.participant;
+
+public record ParticipantRequestPayload(String name, String email) {
+}
